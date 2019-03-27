d8a722c6f082559dbd8b6892c1a9c296af0079d5
Zeno02 <48052787+Zeno02@users.noreply.github.com>
2019-03-27 11:33:21 +0100

    Add files via upload

---
+++ SendMail.ps1
@@ -0,0 +1,8 @@
+﻿$From = "example@example.com"
+$To = "example@example.com"
+$Cc = "example@example.com"
+$Subject = "Enter subject here"
+$Body = "Enter message here"
+$SMTPServer = "Enter SMTP Server here"
+$SMTPPort = "PortNumber"
+Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential (Get-Credential) –DeliveryNotificationOption OnSuccess
