0ca42d0bf8c5e464fbb69ae8148eac0c87be6f88
Zeno02 <48052787+Zeno02@users.noreply.github.com>
2019-05-10 13:46:17 +0200

    Add files via upload

---
+++ Install_IIS.ps1
@@ -0,0 +1,26 @@
+﻿  #Name: IIS_Install
+	#Script URI: https://github.com/Zeno02/Powershell/PCINFO
+	#Description: Installeert IIS
+	#Version: versie 1.0.0
+	#Author: Zeno Schoen
+	#Author URI: https://github.com/Zeno02
+
+
+Try {
+
+Install-WindowsFeature -Name PFSV1 `  -IncludeAllSubFeature `  -IncludeManagementTools
+
+}
+
+Catch {
+
+$From = "zeno_scgoen@hotmail.com"
+$To = "zeno_schoen@hotmail.com"
+$Cc = ""
+$Subject = "IIS Installatie"
+$Body = "IIS is met success geïnstalleerd."
+$SMTPServer = "smtp.office365.com"
+$SMTPPort = "587"
+Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential (Get-Credential) –DeliveryNotificationOption OnSuccess
+
+}
